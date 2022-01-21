1f65764dec122fa4fb2c5228291c62f3a4de1290
DimaBeley <2beyva@gmail.com>
2022-01-21 17:23:46 +0000

    firs sandbox, form

---
+++ src/features/calculator/Calculator.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Styles from './Calculator.module.css';
+
+export function Calculator() {
+    const [screenField, setScreenField] = useState('');
+    
+    const  handleSubmit = (event: React.SyntheticEvent): void => {
+        event.preventDefault();
+        const target = event.target as typeof event.target & {
+            screenField: {value: string}
+        };
+        console.log(target, 'target');
+
+        const {value} = target.screenField;
+
+        setScreenField(screenField.concat(` ${value}`));
+        target.screenField.value = '';
+    }
+
+
+    return (
+        <div>    
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="text" name="screenField"/>
+
+                <input type="submit" value="↵"/>
+            </form>
+            <div className={Styles.Calculator}>
+                <span>{screenField}</span>
+            </div>
+        </div>
+    )
+}
