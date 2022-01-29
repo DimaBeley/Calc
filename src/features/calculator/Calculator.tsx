752cd8050722c741d28babe7f0388de40998853e
DimaBeley <2beyva@gmail.com>
2022-01-29 23:06:59 +0000

    todolist with typescript

--- src/features/calculator/Calculator.tsx
+++ src/features/calculator/Calculator.tsx
@@ -2,16 +2,30 @@ import React, { useState } from 'react';
 import Styles from './Calculator.module.css';
 
 export function Calculator() {
-    const [screenField, setScreenField] = useState('');
+
+    type listItem = {
+        todo: string,
+        id: number
+    }
+    const [todos, setTodos] = useState<listItem[]>([]);
+    const todoList = todos.map((todo, i) => (
+        <li key={todo.id}>{} - {todo.todo} 
+            <button type='button' className={Styles.removeListItemButton} onClick={() => removeListItem(todo.id)}>x</button>
+        </li>
+    ))
+
+    const removeListItem = (index:number):void => {
+        setTodos(todos.filter(todo => todo.id !== index));
+    }
     
-    const  handleSubmit = (event: React.SyntheticEvent): void => {
+    const handleSubmit = (event: React.SyntheticEvent): void => {
         event.preventDefault();
         const target = event.target as typeof event.target & {
             screenField: {value: string}
         };
-        const {value} = target.screenField;
-
-        setScreenField(screenField.concat(` ${value}`));
+        const { value } = target.screenField;
+        const id = todos.length + 1
+        setTodos([...todos, {todo: value, id: id}]);
         target.screenField.value = '';
     }
 
@@ -24,7 +38,9 @@ export function Calculator() {
                 <input type="submit" value="↵"/>
             </form>
             <div className={Styles.Calculator}>
-                <span>{screenField}</span>
+                <ul>
+                    {todoList}
+                </ul>
             </div>
         </div>
     )
